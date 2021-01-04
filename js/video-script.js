fee26c7c136ca47a2a901568bc4454671f1a7fec
Ben <classle55sc2@gmail.com>
2021-01-04 11:49:36 +0000

    potential fix

--- js/video-script.js
+++ js/video-script.js
@@ -5,16 +5,16 @@ var twitchEmbed = new Twitch.Embed("twitch-embed", {
   height: "100%",
   channel: "topleftesports",
 
-  parent: ["localhost", "tlclassless.github.io"],
+  parent: ["localhost", "tlclassless.github.io", "topleftesports.com"],
 });
 
 console.log(twitchEmbed._iframe.attributes[0].nodeValue);
 
 var srcs = [
-  "https://embed.twitch.tv/?channel=topleftesports&height=100%25&migration=true&parent=localhost&referrer=http%3A%2F%2Flocalhost%3A5500%2F&width=100%25",
-  "https://embed.twitch.tv/?channel=sicksteen10&height=100%25&migration=true&parent=localhost&referrer=http%3A%2F%2Flocalhost%3A5500%2F&width=100%25",
-  "https://embed.twitch.tv/?channel=tlclassless&height=100%25&migration=true&parent=localhost&referrer=http%3A%2F%2Flocalhost%3A5500%2F&width=100%25",
-  "https://embed.twitch.tv/?channel=condormankini&height=100%25&migration=true&parent=localhost&referrer=http%3A%2F%2Flocalhost%3A5500%2F&width=100%25",
+  "https://embed.twitch.tv/?channel=topleftesports&height=100%25&migration=true&parent=tlclassless.github.io&referrer=http%3A%2F%tlclassless.github.io%3A5500%2F&width=100%25",
+  "https://embed.twitch.tv/?channel=sicksteen10&height=100%25&migration=true&parent=tlclassless.github.io&referrer=http%3A%2F%tlclassless.github.io%3A5500%2F&width=100%25",
+  "https://embed.twitch.tv/?channel=tlclassless&height=100%25&migration=true&parent=tlclassless.github.io&referrer=http%3A%2F%tlclassless.github.io%3A5500%2F&width=100%25",
+  "https://embed.twitch.tv/?channel=condormankini&height=100%25&migration=true&parent=tlclassless.github.io&referrer=http%3A%2F%tlclassless.github.io%3A5500%2F&width=100%25",
 ];
 
 var playerIco = document.getElementsByClassName("switch-ico");
